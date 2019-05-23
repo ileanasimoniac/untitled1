423d9de733547205efbf14f41b3960a7637f5c44
ileeana <simonileana5@gmail.com>
2019-05-23 21:49:19 +0300

    functiile care este mai mic si care este mai mare

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("a=");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("b=");
+        int b = new Scanner(System.in).nextInt();
+
+        Main.careEsteMaiMare(a, b);
+        Main.careEsteMaiMic(a, b);
+    }
+
+    private static void careEsteMaiMic(int a, int b) {
+        if (a < b) {
+            System.out.println("a este mai mic decat b");
+        } else if(b < a) {
+            System.out.println("b este mai mic decat a");
+        }
+    }
+
+    private static void careEsteMaiMare(int a, int b) {
+        if  (a>b) {
+            System.out.println("a  este mai mare  decat b");
+        } else if (b>a) {
+            System.out.println("b  este mai mare  decat a");
+
+        }
+
     }
 }
